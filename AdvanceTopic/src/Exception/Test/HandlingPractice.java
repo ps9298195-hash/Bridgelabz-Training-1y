a07e2eff3a4e1ebccc78bdf06459de3b06ace7c8
Ekta <ps9298195@gmail.com>
2026-03-24 14:02:06 +0530

    05-String

---
+++ AdvanceTopic/src/Exception/Test/HandlingPractice.java
@@ -0,0 +1,16 @@
+package Exception.Test;
+
+public class age {
+    static void ageChecker(int age)throws ArithmeticException,NullPointerException{
+        if(age<18)
+            throw new ArithmeticException("Invalid age");
+
+    }
+    public static void main(String []args){
+        try{
+            ageChecker(10);
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
